fbc3e4548f3dc9a6568c84ad6e323cfa6dd37336
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-06-25 18:12:04 +0530

    Firebase Added

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcXckpp0HDeAot_98IXqdVL_k70Pyg2Yo",
+  authDomain: "gold-s-gym-381df.firebaseapp.com",
+  projectId: "gold-s-gym-381df",
+  storageBucket: "gold-s-gym-381df.appspot.com",
+  messagingSenderId: "36181026669",
+  appId: "1:36181026669:web:531fce4a0b2a333a931c64"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
